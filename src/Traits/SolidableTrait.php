7ad86808c938354dfc2aaaee824d72ee8a15b6fd
Wolfy-J <wolfy.jd@gmail.com>
2019-04-25 11:45:54 +0300

    psr-12

--- src/Traits/SolidableTrait.php
+++ src/Traits/SolidableTrait.php
@@ -1,10 +1,11 @@
 <?php
-declare(strict_types=1);
 /**
- * Spiral, Core Components
+ * Spiral Framework.
  *
- * @author Wolfy-J
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
  */
+declare(strict_types=1);
 
 namespace Spiral\Models\Traits;
 
