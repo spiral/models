956aa7f22c731bf57f137f6c2ddb99a736c016b8
spiralbot <ci-bot@spiralscout.com>
2025-01-24 10:34:00 +0000

    Merge pull request #1207: Apply Spiral Code Style

--- tests/DataEntityTest.php
+++ tests/DataEntityTest.php
@@ -13,25 +13,25 @@ class DataEntityTest extends TestCase
     {
         $entity = new DataEntity();
         $entity->setField('abc', 123);
-        $this->assertEquals(123, $entity->getField('abc'));
+        self::assertEquals(123, $entity->getField('abc'));
 
-        $this->assertTrue($entity->hasField('abc'));
-        $this->assertFalse($entity->hasField('bce'));
+        self::assertTrue($entity->hasField('abc'));
+        self::assertFalse($entity->hasField('bce'));
     }
 
     public function testMagicProperties(): void
     {
         $entity = new DataEntity();
         $entity->abc = 123;
-        $this->assertEquals(123, $entity->abc);
+        self::assertSame(123, $entity->abc);
 
-        $this->assertTrue(isset($entity->abc));
+        self::assertTrue(isset($entity->abc));
     }
 
     public function testPackingSimple(): void
     {
         $entity = new DataEntity(['a' => 'b', 'c' => 10]);
-        $this->assertSame(['a' => 'b', 'c' => 10], $entity->getValue());
+        self::assertSame(['a' => 'b', 'c' => 10], $entity->getValue());
     }
 
     public function testSerialize(): void
@@ -39,7 +39,7 @@ class DataEntityTest extends TestCase
         $data = ['a' => 123, 'b' => null, 'c' => 'test'];
 
         $entity = new DataEntity($data);
-        $this->assertEquals($data, $entity->getValue());
+        self::assertEquals($data, $entity->getValue());
     }
 
     public function testSetValue(): void
@@ -47,37 +47,37 @@ class DataEntityTest extends TestCase
         $data = ['a' => 123, 'b' => null, 'c' => 'test'];
 
         $entity = new PublicEntity($data);
-        $this->assertEquals($data, $entity->getValue());
+        self::assertEquals($data, $entity->getValue());
 
         $entity = new PublicEntity();
         $entity->setValue(['a' => 123]);
-        $this->assertEquals(['a' => 123], $entity->getValue());
+        self::assertSame(['a' => 123], $entity->getValue());
 
-        $this->assertSame(['a'], $entity->getKeys());
-        $this->assertTrue(isset($entity->a));
+        self::assertSame(['a'], $entity->getKeys());
+        self::assertTrue(isset($entity->a));
 
         unset($entity->a);
-        $this->assertEquals([], $entity->getValue());
+        self::assertSame([], $entity->getValue());
 
         $entity['a'] = 90;
-        $this->assertEquals(['a' => 90], $entity->getValue());
-        $this->assertSame(90, $entity['a']);
-        $this->assertTrue(isset($entity['a']));
+        self::assertSame(['a' => 90], $entity->getValue());
+        self::assertSame(90, $entity['a']);
+        self::assertArrayHasKey('a', $entity);
 
         unset($entity['a']);
-        $this->assertEquals([], $entity->getValue());
+        self::assertSame([], $entity->getValue());
 
         $entity['a'] = 90;
         foreach ($entity as $key => $value) {
-            $this->assertSame('a', $key);
-            $this->assertSame(90, $value);
+            self::assertSame('a', $key);
+            self::assertSame(90, $value);
         }
 
-        $this->assertSame('a', $key);
-        $this->assertSame(90, $value);
+        self::assertSame('a', $key);
+        self::assertSame(90, $value);
 
-        $this->assertEquals(['a' => 90], $entity->toArray());
-        $this->assertEquals(['a' => 90], $entity->jsonSerialize());
+        self::assertSame(['a' => 90], $entity->toArray());
+        self::assertSame(['a' => 90], $entity->jsonSerialize());
     }
 
     public function testSecured(): void
@@ -85,19 +85,19 @@ class DataEntityTest extends TestCase
         $entity = new SecuredEntity();
         $entity->setValue([
             'name' => 'Antony',
-            'id'   => '900'
+            'id'   => '900',
         ]);
 
-        $this->assertEquals([], $entity->getValue());
+        self::assertSame([], $entity->getValue());
 
         $entity = new PartiallySecuredEntity();
         $entity->setValue([
             'name' => 'Antony',
-            'id'   => 900
+            'id'   => 900,
         ]);
 
-        $this->assertEquals([
-            'id' => 900
+        self::assertSame([
+            'id' => 900,
         ], $entity->getValue());
     }
 
@@ -106,17 +106,17 @@ class DataEntityTest extends TestCase
         $entity = new FilteredEntity();
         $entity->setValue([
             'name' => 'Antony',
-            'id'   => '900'
+            'id'   => '900',
         ]);
 
-        $this->assertEquals([
-            'id' => 900
+        self::assertSame([
+            'id' => 900,
         ], $entity->getValue());
 
         $entity->id = [];
 
-        $this->assertEquals([
-            'id' => 0
+        self::assertSame([
+            'id' => 0,
         ], $entity->getValue());
     }
 
@@ -125,46 +125,46 @@ class DataEntityTest extends TestCase
         $entity = new NullableEntity();
         $entity->setValue([
             'name' => 'Antony',
-            'id'   => '900'
+            'id'   => '900',
         ]);
 
-        $this->assertEquals([
+        self::assertSame([
             'name' => 'Antony',
-            'id'   => 900
+            'id'   => 900,
         ], $entity->getValue());
 
         // no filter
         $entity->name = null;
-        $this->assertEquals([
+        self::assertEquals([
             'name' => null,
-            'id'   => 900
+            'id'   => 900,
         ], $entity->getValue());
 
         $entity->id = null;
-        $this->assertEquals([
+        self::assertEquals([
             'name' => null,
-            'id'   => null
+            'id'   => null,
         ], $entity->getValue());
 
 
         $entity = new FilteredEntity();
         $entity->setValue([
             'name' => 'Antony',
-            'id'   => null
+            'id'   => null,
         ]);
 
-        $this->assertEquals([
-            'id' => 0
+        self::assertSame([
+            'id' => 0,
         ], $entity->getValue());
     }
 
     public function testGetters(): void
     {
         $entity = new GetEntity(['id' => []]);
-        $this->assertSame(0, $entity->id);
+        self::assertSame(0, $entity->id);
 
-        $this->assertEquals([
-            'id' => 0
+        self::assertSame([
+            'id' => 0,
         ], $entity->getFields());
     }
 }
